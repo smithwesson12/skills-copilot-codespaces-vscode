a34d2184827bdcd86a5f98818df0a54a557e92b6
smithwesson12 <131662503+smithwesson12@users.noreply.github.com>
2023-08-02 15:53:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var memberBtn = document.getElementById("memberBtn");
+    var skillsBtn = document.getElementById("skillsBtn");
+    var projectsBtn = document.getElementById("projectsBtn");
+    var contactBtn = document.getElementById("contactBtn");
+    var memberBtn2 = document.getElementById("memberBtn2");
+    var skillsBtn2 = document.getElementById("skillsBtn2");
+    var projectsBtn2 = document.getElementById("projectsBtn2");
+    var contactBtn2 = document.getElementById("contactBtn2");
+    member.style.display = "block";
+    skills.style.display = "none";
+    projects.style.display = "none";
+    contact.style.display = "none";
+    memberBtn.style.backgroundColor = "#f1f1f1";
+    skillsBtn.style.backgroundColor = "#fff";
+    projectsBtn.style.backgroundColor = "#fff";
+    contactBtn.style.backgroundColor = "#fff";
+    memberBtn2.style.backgroundColor = "#f1f1f1";
+    skillsBtn2.style.backgroundColor = "#fff";
+    projectsBtn2.style.backgroundColor = "#fff";
+    contactBtn2.style.backgroundColor = "#fff";
+}
